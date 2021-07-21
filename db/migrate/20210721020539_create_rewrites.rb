8ba002f53467793a1250a648cb40cddbf3e84688
camryn williams <williams.camryn.e@gmail.com>
2021-07-20 22:14:02 -0400

    models created

---
+++ db/migrate/20210721020539_create_rewrites.rb
@@ -0,0 +1,12 @@
+class CreateRewrites < ActiveRecord::Migration[6.1]
+  def change
+    create_table :rewrites do |t|
+      t.string :title
+      t.string :rewritten_lyrics
+      t.integer :song_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20210721020641_create_songs.rb
@@ -0,0 +1,10 @@
+class CreateSongs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :original_lyrics
+
+      t.timestamps
+    end
+  end
+end
