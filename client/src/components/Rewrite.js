0bbb5097d530d549a5330e7fff4e19ffdd790c28
camryn williams <williams.camryn.e@gmail.com>
2021-07-28 20:31:38 -0400

    adds state to rewrite component

---
+++ client/src/components/Rewrite.js
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+
+const Rewrite = () => {
+    const [title, setTitle] = useState('')
+    const [rewritten_lyrics, setRewrittenLyrics] = useState('')
+}
+
+export default Rewrite
