2e2f0188f86c575e6cac2744c89f2b0dfc86ad13
camryn williams <williams.camryn.e@gmail.com>
2021-07-29 11:29:45 -0400

    adds state & history to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import Home from './components/Home'
+import NavBar from './components/NavBar';
 
 function App() {
+
+  const [user, setUser] = useState({})
+  const [loggedIn, setLoggedIn] = useState(false)
+  const history = useHistory();
 
   return (
     <div>
       <Router>
+      <NavBar/>
         <div>
           <Route path="/" component={Home}/>
         </div>
