15ef113f6857b513223172261facf85efcfb1238
camryn williams <williams.camryn.e@gmail.com>
2021-07-28 20:16:22 -0400

    adds state to songs page

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import { NavLink } from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NavBar
---
+++ client/src/components/SongsPage.js
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+
+const SongsPage = () => {
+    const [songs, setSongs] = useState([])
+
+}
+
+export default SongsPage
