3598cf02bdb201dcc4c0c7db06a37aa1a316a7c7
camryn williams <williams.camryn.e@gmail.com>
2021-07-28 20:25:48 -0400

    adds title to state in song page

--- client/src/components/Song.js
+++ client/src/components/Song.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 
 const Song = () => {
+    const [title, setTitle] = useState('');
     const [rewrites, setRewrites] = useState([])
 
+    
 }
 
 export default Song
